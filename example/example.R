ae99fba604920d6128c38abc76eab0359bffb613
Corneliu Cofaru <cornel@oxoaresearch.com>
2026-05-05 22:27:18 +0200

    Update example

--- example/example.R
+++ example/example.R
@@ -1,6 +1,11 @@
 library(glmmTMB)
 data_path <- "~/projects/DataLinter/test/data/imbalanced_data.csv"
+#data_path <- "~/projects/DataLinter/test/data/correlated_target_data.csv"
+#data_path <- "~/projects/DataLinter/test/data/correlated_data.csv"
 out1 <- read.csv(data_path, header=TRUE)
 m2 <- glmmTMB(col4 ~ col1 + col2 + col3,
               data = out1,
-              family=binomial(link="linear"))
+              family=binomial(link=linear))
+print(m2)
+m3 <- glm(col4~col1+col2, data=out1, family="binomial")
+print(m3)
